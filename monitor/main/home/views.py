f75515efdcd271dfa07868394990a95392a5fed0
lichen <lichen6211@163.com>
2018-05-25 13:09:21 +0800

    资讯获取

--- monitor/main/home/views.py
+++ monitor/main/home/views.py
@@ -22,29 +22,16 @@ def get_all_information():
 @mod.route('/every_infos/',methods=['POST'])
 def get_information():
     type = request.form.get('type','')
-    # name_id = request.form.get('id','')
-    # name = request.form.get('name','')
-    # department = request.form.get('department','')
-    # job = request.form.get('job','')
-    # content = {}
-    # if name == '' and department == '' and job == '':
-    #     content['id'] = name_id
-    # elif name != '' and department != '' and job != '':
-    #     content['id'] = name_id
-    #     content['name'] = name
-    #     content['department'] = department
-    #     content['job'] = job
     content = eval(request.form.get('content',''))
     if type == '' or type == None:
         return jsonify({"message":"输入类型"}),400
     elif content == '' or content == None:
         return jsonify({"message":"输入条件"}),400
     else:
-        result,tag = get_everyinformation(type,content)
-        if tag == '1':
-            return jsonify({"message":result}),200
-        if tag == '0':
-            return jsonify({"message":result}),400
+        result = get_everyinformation(type,content)
+        if result == 0:
+            return jsonify({"message":"The time field is in the wrong format"}),401
+        return jsonify({"message":result})
 
 #获取地区基本信息
 @mod.route('/gov_area/')
