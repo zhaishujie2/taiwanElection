31e4ea3463ffbc9e12e17391cbdbe9a40dac313b
zhaishujie2 <17789624306@163.com>
2018-05-31 17:53:22 +0800

    上传图片！！地区信息历届信息

--- monitor/main/system/operate_info.py
+++ monitor/main/system/operate_info.py
@@ -828,6 +828,435 @@ def update_people_information(info_type,content):
     except Exception as erro:
         app.logger.error(erro)
         return 0
+
+#地区信息写入数据
+def insert_area_info(administrative_id, area_info, governance_situation, year):
+    conn = getconn()
+    cur = conn.cursor()
+
+    insert_sql = """INSERT INTO `administrative_infos` (`administrative_id`,`area_info`,`governance_situation`,`year`) VALUES (%s,%s,%s,%s)"""
+    select_sql = """SELECT `info_id`  FROM `administrative_infos` WHERE `administrative_id` = %s AND `area_info`= %s AND `governance_situation`= %s AND `year`= %s"""
+    try:
+        insert_re = ''
+        select_re = cur.execute(select_sql, (administrative_id, area_info, governance_situation, year))
+        if select_re != 0:
+            return 406
+        elif select_re == 0:
+            insert_re = cur.execute(insert_sql, (administrative_id, area_info, governance_situation, year))
+        if insert_re < 1:
+            return 0
+        else:
+            return 1
+    except Exception as erro:
+        app.logger.error(erro)
+        return 0
+    finally:
+        closeAll(conn, cur)
+
+
+#地区信息删除数据
+def delete_area_info(data):
+    conn = getconn()
+    cur = conn.cursor()
+    info_id = data.get('info_id')
+    try:
+        delete_sql = """DELETE FROM `administrative_infos` WHERE info_id = %s """
+        re = cur.execute(delete_sql,(info_id))
+        if re < 1:
+            return 0
+        else:
+            return 1
+    except Exception as erro:
+        app.logger.error(erro)
+        return 0
+    finally:
+        closeAll(conn, cur)
+
+
+# 地区信息更改数据
+def update_info_area(datas):
+
+    where_dict = datas.get('where_dict')
+    up_dict = datas.get('up_dict')
+    if len(where_dict) == 0 or len(up_dict) == 0:
+        if len(where_dict) == 0:
+            return "更新条件不能为空"
+        if len(up_dict) == 0:
+            return "更新内容不能为空"
+    else:
+        conn = getconn()
+        cur = conn.cursor()
+        try:
+            where_sql = ''
+            up_sql = ''
+            where_num = 0
+            up_num = 0
+            print(where_dict.items())
+            for k,v in where_dict.items():
+                where_num += 1
+                if where_num < len(where_dict):
+                    where_sql += "{} = '{}' AND ".format(k,v)
+                else:
+                    where_sql += "{} = '{}'".format(k,v)
+            for k,v in up_dict.items():
+                up_num += 1
+                if up_num < len(up_dict):
+                    up_sql += "{} = '{}', ".format(k,v)
+                else:
+                    up_sql += "{} = '{}' ".format(k,v)
+
+            update_sql = """UPDATE `administrative_infos` SET """ + up_sql +' WHERE '+ where_sql
+            re = cur.execute(update_sql)
+            if re < 1 :
+                return 0
+            else:
+                return 1
+        except Exception as erro:
+            app.logger.error(erro)
+            return 0
+        finally:
+            closeAll(conn, cur)
+
+
+#地区信息查询所有数据
+def select_area_info():
+    conn = getconn()
+    cur = conn.cursor()
+
+    try:
+        select_sql = """SELECT `info_id`,`administrative_id` ,`area_info` ,`governance_situation` ,`year`  FROM `administrative_infos` """
+        re = cur.execute(select_sql)
+        if re < 1:
+            return 0
+        else:
+            select_admini_name()
+            result = cur.fetchall()
+            lists = []
+            for item in result:
+                # pro_name = select_pro_by_id(item[1])
+                dict = {}
+                for k, v in admini_id_name.items():
+                    if item[1] == k:
+                        dict['administrative_name'] = admini_id_name[item[1]]
+                dict['info_id'] = item[0]
+                dict['administrative_id'] = item[1]
+                # dict['administrative_name'] = pro_name
+                dict['area_info'] = item[2]
+                dict['governance_situation'] = item[3]
+                dict['year'] = item[4]
+                lists.append(dict)
+            return lists
+    except Exception as erro:
+        app.logger.error(erro)
+        return 0
+    finally:
+        closeAll(conn, cur)
+
+
+#地区信息查询一条数据
+def select_area_info_one(data):
+    conn = getconn()
+    cur = conn.cursor()
+
+    info_id = data['info_id']
+    try:
+        select_sql = """SELECT `info_id`,`administrative_id` ,`area_info` ,`governance_situation` ,`year`  FROM `administrative_infos` WHERE `info_id` = %s """
+
+        re = cur.execute(select_sql, (info_id))
+        if re < 1:
+            return 0
+        else:
+            select_admini_name()
+            item = cur.fetchone()
+            # pro_name = select_pro_by_id(item[1])
+            dict = {}
+            for k, v in admini_id_name.items():
+                if item[1] == k:
+                    dict['administrative_name'] = admini_id_name[item[1]]
+            dict['info_id'] = item[0]
+            dict['administrative_id'] = item[1]
+            # dict['administrative_name'] = pro_name
+            dict['area_info'] = item[2]
+            dict['governance_situation'] = item[3]
+            dict['year'] = item[4]
+            # print(dict)
+            return dict
+    except Exception as erro:
+        app.logger.error(erro)
+        return 0
+    finally:
+        closeAll(conn, cur)
+
+
+#分页查询地区信息数据
+def select_area_info_page(page, count):
+    conn = getconn()
+    cur = conn.cursor()
+    try:
+        select_info = '''SELECT `info_id`,`administrative_id`,`area_info`,`governance_situation`,`year` FROM `administrative_infos` LIMIT %s, %s;'''
+        count = count
+        if int(page) == 1 or int(page) == 0:
+            page_t = 0
+        else:
+            page_t = (int(page) - 1) * int(count)
+        re = cur.execute(select_info,(int(page_t),int(count)))
+        if re < 1:
+            return 0
+        else:
+            select_admini_name()
+            result = cur.fetchall()
+            lists = []
+            for item in result:
+                dict = {}
+                for k, v in admini_id_name.items():
+                    if item[1] == k:
+                        dict['administrative_name'] = admini_id_name[item[1]]
+                # pro_name = select_pro_by_id(item[1])
+                dict['info_id'] = item[0]
+                dict['administrative_id'] = item[1]
+                dict['area_info'] = item[2]
+                dict['governance_situation'] = item[3]
+                dict['year'] = item[4]
+
+                lists.append(dict)
+            return lists
+    except Exception as erro:
+        app.logger.error(erro)
+        return 0
+    finally:
+        closeAll(conn, cur)
+
+
+#历届选举信息写入数据
+def insert_election_info(administrative_id, elector, election_score, election_parties, period, year):
+    conn = getconn()
+    cur = conn.cursor()
+
+    insert_sql = """INSERT INTO `previous_elections` (`administrative_id`,`elector`,`election_score`,`election_parties`,`period`,`year`) VALUES (%s,%s,%s,%s,%s,%s)"""
+    select_sql = """SELECT `id`  FROM `previous_elections` WHERE `administrative_id` = %s AND `elector`= %s AND `election_score`= %s AND `election_parties`= %s AND `period`= %s AND `year`= %s"""
+    try:
+        # print(insert_sql)
+        insert_re = ''
+        select_re = cur.execute(select_sql, (administrative_id, elector, election_score, election_parties, period, year))
+        if select_re != 0:
+            return 406
+        elif select_re == 0:
+            insert_re = cur.execute(insert_sql, (administrative_id, elector, election_score, election_parties, period, year))
+        # print(re)
+        if insert_re < 1:
+            return 0
+        else:
+            return 1
+    except Exception as erro:
+        app.logger.error(erro)
+        return 0
+    finally:
+        closeAll(conn, cur)
+
+
+#历届选举信息删除数据
+def delete_election_info(data):
+    conn = getconn()
+    cur = conn.cursor()
+    id = data.get('id')
+    try:
+        delete_sql = """DELETE FROM `previous_elections` WHERE id = %s """
+        re = cur.execute(delete_sql,(id))
+        if re < 1:
+            return 0
+        else:
+            return 1
+    except Exception as erro:
+        app.logger.error(erro)
+        return 0
+    finally:
+        closeAll(conn, cur)
+
+
+# 更改历届选举信息数据
+def update_election_info(datas):
+
+    where_dict = datas.get('where_dict')
+    up_dict = datas.get('up_dict')
+    if len(where_dict) == 0 or len(up_dict) == 0:
+        if len(where_dict) == 0:
+            return "更新条件不能为空"
+        if len(up_dict) == 0:
+            return "更新内容不能为空"
+    else:
+        conn = getconn()
+        cur = conn.cursor()
+        try:
+            where_sql = ''
+            up_sql = ''
+            where_num = 0
+            up_num = 0
+            for k,v in where_dict.items():
+                where_num += 1
+                if where_num < len(where_dict):
+                    where_sql += "{} = '{}' AND ".format(k,v)
+                else:
+                    where_sql += "{} = '{}'".format(k,v)
+            for k,v in up_dict.items():
+                up_num += 1
+                if up_num < len(up_dict):
+                    up_sql += "{} = '{}', ".format(k,v)
+                else:
+                    up_sql += "{} = '{}' ".format(k,v)
+
+            update_sql = """UPDATE `previous_elections` SET """ + up_sql +' WHERE '+ where_sql
+            # print(update_sql)
+            re = cur.execute(update_sql)
+            if re < 1:
+                return 0
+            else:
+                return 1
+        except Exception as erro:
+            app.logger.error(erro)
+            return 0
+        finally:
+            closeAll(conn, cur)
+
+
+# 查询历届选举信息数据所有数据
+def select_election_all():
+    conn = getconn()
+    cur = conn.cursor()
+
+    try:
+        select_sql = """SELECT `id`,`administrative_id` ,`elector` ,`election_score` ,`election_parties` ,`period` ,`year` FROM `previous_elections` """
+        re = cur.execute(select_sql)
+        if re < 1:
+            return 0
+        else:
+            # select_admini_name()
+            result = cur.fetchall()
+            lists = []
+            for item in result:
+                dict = {}
+                for k, v in admini_id_name.items():
+                    if item[1] == k:
+                        dict['administrative_name'] = admini_id_name[item[1]]
+                        # print(admini_id_name[k])
+                dict['id'] = item[0]
+                dict['administrative_id'] = item[1]
+                dict['elector'] = item[2]
+                dict['election_score'] = item[3]
+                dict['election_parties'] = item[4]
+                dict['period'] = item[5]
+                dict['year'] = item[6]
+
+                lists.append(dict)
+            return lists
+    except Exception as erro:
+        app.logger.error(erro)
+        return 0
+    finally:
+        closeAll(conn,cur)
+
+
+#历届选举信息根据id查询单条数据
+def select_election_info_one(data):
+    conn = getconn()
+    cur = conn.cursor()
+
+    id = data['id']
+    try:
+        select_sql = """SELECT `id`,`administrative_id` ,`elector` ,`election_score` ,`election_parties` ,`period` ,`year` FROM `previous_elections` WHERE `id` = %s """
+        re = cur.execute(select_sql, (id))
+        if re < 1:
+            return 0
+        else:
+            select_admini_name()
+            item = cur.fetchone()
+            dict = {}
+            for k, v in admini_id_name.items():
+                if item[1] == k:
+                    dict['administrative_name'] = admini_id_name[item[1]]
+            dict['id'] = item[0]
+            dict['administrative_id'] = item[1]
+            # dict['administrative_name'] = pro_name
+            dict['elector'] = item[2]
+            dict['election_score'] = item[3]
+            dict['election_parties'] = item[4]
+            dict['period'] = item[5]
+            dict['year'] = item[6]
+
+            return dict
+    except Exception as erro:
+        app.logger.error(erro)
+        return 0
+    finally:
+        closeAll(conn,cur)
+
+
+#分页查询历届选举信息情况数据
+def select_election_info_page(page, count):
+    conn = getconn()
+    cur = conn.cursor()
+    try:
+        select_info = '''SELECT `id`,`administrative_id`,`elector`,`election_score`,`election_parties`,`period`,`year` FROM `previous_elections` LIMIT %s, %s;'''
+        # select_sql2 = """SELECT `*` FROM `previous_elections` """
+        # re2 = cur.execute(select_sql2)
+        # all_pages = re2 / int(count)
+        # if all_pages < 1:
+        #     all_pages = 1
+        # else:
+        #     all_pages = math.ceil(all_pages)
+        count = count
+        if int(page) == 1 or int(page) == 0:
+            page_t = 0
+        else:
+            page_t = (int(page) - 1) * int(count)
+        re = cur.execute(select_info,(int(page_t),int(count)))
+        if re < 1:
+            return 0
+        else:
+            select_admini_name()
+            result = cur.fetchall()
+            lists = []
+            for item in result:
+                # pro_name = select_pro_by_id(item[1])
+                dict = {}
+                for k, v in admini_id_name.items():
+                    if item[1] == k:
+                        dict['administrative_name'] = admini_id_name[item[1]]
+                dict['id'] = item[0]
+                dict['administrative_id'] = item[1]
+                # dict['administrative_name'] = pro_name
+                dict['elector'] = item[2]
+                dict['election_score'] = item[3]
+                dict['election_parties'] = item[4]
+                dict['period'] = item[5]
+                dict['year'] = item[6]
+                # dict['all_pages'] = all_pages
+                lists.append(dict)
+            return lists
+    except Exception as erro:
+        app.logger.error(erro)
+        return 0
+    finally:
+        closeAll(conn, cur)
+
+
+admini_id_name = {}
+def select_admini_name():
+
+    conn = getconn()
+    cur = conn.cursor()
+
+    try:
+        select_sql = """SELECT `administrative_id`, `administrative_name` FROM `administrative_area` """
+        re = cur.execute(select_sql)
+        if re:
+            items = cur.fetchall()
+            for item in items:
+                admini_id_name[item[0]] = item[1]
+    except Exception as erro:
+        app.logger.error(erro)
+        return 0
+    finally:
+        closeAll(conn, cur)
 # if __name__ == '__main__':
 #     # a = insert_info('7017','李晨','2018','www.facebook.com')
 #     a = delete_info(11)#{"administrative_id":"7019","administrative_name":"李晨"}
--- monitor/main/system/views.py
+++ monitor/main/system/views.py
@@ -1,9 +1,12 @@
 # coding=utf-8
-from flask import jsonify,request
+from flask import jsonify,request,url_for,send_from_directory
 from . import mod
 from monitor import app
-from monitor.main.system.operate_info import insert_info,delete_info,update_info,select_info,get_is_candidate,get_region_dict,get_one_infos,select_candidate_facebook,add_administrative_infos,insert_candidate_facebook,delete_people_information,update_people_information
-import json
+from monitor.main.system.operate_info import insert_info,delete_info,update_info,select_info,get_is_candidate,get_region_dict,get_one_infos,select_candidate_facebook,add_administrative_infos,insert_candidate_facebook,delete_people_information,update_people_information,get_everyinformation,get_all_years,insert_img,get_pages,allowed_file, \
+    delete_area_info, insert_area_info, update_info_area, select_area_info, select_area_info_one, select_area_info_page, \
+    insert_election_info, delete_election_info, update_election_info, select_election_all, select_election_info_one, \
+    select_election_info_page
+import json,os
 
 #facebook写入数据
 @mod.route('/insert/',methods=['POST'])
@@ -131,10 +134,16 @@ def region_dict():
         return str(0)
 
 #做分页
-@mod.route('/get_pages/',methods = ['POST'])
+@mod.route('/pages/',methods = ['POST'])
 def pages():
-    data = request.form.get('data','')
-    pass
+    try:
+        result = get_pages()
+        if result == 0:
+            return jsonify({"message":"The data is wrong "}),400
+        return jsonify({"message":result}),200
+    except Exception as erro:
+        app.logger.error(erro)
+        return str(0)
 
 #地区信息
 def area_infos():
@@ -211,6 +220,299 @@ def delete_information():
                 return jsonify({"message": "The data is wrong"}), 400
             else:
                 return jsonify({"message": result}),200
+    except Exception as erro:
+        app.logger.error(erro)
+        return str(0)
+
+#获取个人信息
+@mod.route('/every_infos/',methods=['POST'])
+def get_information():
+    try:
+        datas = request.form.get('data','')
+        data = json.loads(datas)
+        info_type = data.get('type')
+        content = data.get('content')
+        if info_type == '' or info_type == None:
+            return jsonify({"message":"type input is null"}),406
+        elif content == '' or content == None:
+            return jsonify({"message":"data input is null"}),406
+        else:
+            result = get_everyinformation(info_type,content)
+            if result == 0:
+                return jsonify({"message":"The data is wrong "}),400
+            return jsonify({"message":result}),200
+    except Exception as erro:
+        app.logger.error(erro)
+        return str(0)
+
+#根据地区返回该地区的所有年份
+@mod.route('/years_candidates/',methods=['POST'])
+def years():
+    try:
+        datas = request.form.get('data','')
+        if datas == '' or datas == None:
+            return jsonify({"message":"data input is null"}),406
+        else:
+            data = json.loads(datas)
+            info_type = data.get('type')
+            result = get_all_years(info_type,data)
+            if result == 0:
+                return jsonify({"message":"The data is wrong "}),400
+            return jsonify({"message":result}),200
+    except Exception as erro:
+        app.logger.error(erro)
+        return str(0)
+
+@mod.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+#上传图片
+@mod.route('/image/',methods=['POST'])
+def upload_file():
+    try:
+        file = request.files['file']
+        if file == '' or file == None:
+            return jsonify({"message":"data input is null"}),406
+        if file and allowed_file(file.filename):
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+            file_url = url_for('system.uploaded_file', filename=file.filename)
+            return jsonify({"message":1}),201
+    except Exception as erro:
+        app.logger.error(erro)
+        return str(0)
+
+
+# 地区信息写入数据
+@mod.route('/insert_area/',methods=['POST'])
+def insert_area():
+    try:
+        datas = request.form.get('data', '')
+        datas_info = json.loads(datas)
+        administrative_id = datas_info.get('administrative_id')
+        area_info = datas_info.get('area_info')
+        governance_situation = datas_info.get('governance_situation')
+        year = datas_info.get('year')
+
+        if administrative_id and area_info and governance_situation and year:
+            message = insert_area_info(administrative_id, area_info, governance_situation, year)
+            if message:
+                return jsonify({"message": message}), 200
+            else:
+                return jsonify({"message": "The data field is in the wrong"}), 400
+        else:
+            return jsonify({"message": "input info is not enough"}), 406
+
+    except Exception as erro:
+        app.logger.error(erro)
+        return str(0)
+
+
+# 地区信息删除数据
+@mod.route('/delete_area/',methods=['POST'])
+def delete_area():
+    try:
+        data = request.form.get('data', '')
+
+        if data == '':
+            return jsonify({"message": "type input is null"}), 406
+        else:
+            message = delete_area_info(json.loads(data))
+            if message:
+                return jsonify({"message": message}), 200
+            else:
+                return jsonify({"message": "The data field is in the wrong"}), 400
+
+    except Exception as erro:
+        app.logger.error(erro)
+        return str(0)
+
+
+# 地区信息修改数据
+@mod.route('/update_area/',methods=['POST'])
+def update_area():
+    try:
+        datas = request.form.get('data', '')
+        if datas == '':
+            return jsonify({"message": "type input is null"}), 406
+        else:
+            message = update_info_area(json.loads(datas))
+            if message:
+                return jsonify({"message": message}), 200
+            else:
+                return jsonify({"message": "The data field is in the wrong"}), 400
+
+    except Exception as erro:
+        app.logger.error(erro)
+        return str(0)
+
+
+# 地区信息搜索所有数据
+@mod.route('/select_area/',methods=['GET'])
+def select_area():
+    try:
+        message = select_area_info()
+        if message:
+            return jsonify({"message": message}), 200
+        else:
+            return jsonify({"message": "The data field is in the wrong"}), 400
+    except Exception as erro:
+        app.logger.error(erro)
+        return str(0)
+
+
+# 地区信息根据id查询一条数据
+@mod.route('/select_area_one/', methods=['POST'])
+def select_area_one():
+    try:
+        datas = request.form.get('data', '')
+        if datas == '':
+            return jsonify({"message": "type input is null"}), 406
+        else:
+            message = select_area_info_one(json.loads(datas))
+            if message:
+                return jsonify({"message":message}), 200
+            else:
+                return jsonify({"message": "The data field is in the wrong"}), 400
+    except Exception as erro:
+        app.logger.error(erro)
+        return str(0)
+
+
+# 分页查询地区信息数据
+@mod.route('/select_area_page/', methods=['POST'])
+def select_area_page():
+    try:
+        datas = request.form.get('data','')
+        datas_info = json.loads(datas)
+        page = datas_info['page']
+        count = datas_info['count']
+
+        if page and count:
+            message = select_area_info_page(page, count)
+            return jsonify({"message": message}), 200
+        else:
+            return jsonify({"message": "type input is null"}), 406
+    except Exception as erro:
+        app.logger.error(erro)
+        return str(0)
+
+
+# 增加历届选举信息
+@mod.route('/insert_election/', methods=['POST'])
+def insert_election():
+    try:
+        datas = request.form.get('data', '')
+        datas_info = json.loads(datas)
+        administrative_id = datas_info.get('administrative_id')
+        elector = datas_info.get('elector')
+        election_score = datas_info.get('election_score')
+        election_parties = datas_info.get('election_parties')
+        period = datas_info.get('period')
+        year = datas_info.get('year')
+
+        if administrative_id and elector and election_score and election_parties and period and year:
+            message = insert_election_info(administrative_id, elector, election_score, election_parties, period, year)
+            if message:
+                return jsonify({"message": message}), 200
+            else:
+                return jsonify({"message": "The data field is in the wrong"}), 400
+        else:
+            return jsonify({"message": "input info is not enough"}), 406
+
+    except Exception as erro:
+        app.logger.error(erro)
+        return str(0)
+
+
+# 删除历届选举信息
+@mod.route('/delete_election/', methods=['POST'])
+def delete_election():
+    try:
+        data = request.form.get('data', '')
+
+        if data == '':
+            return jsonify({"message": "type input is null"}), 406
+        else:
+            message = delete_election_info(json.loads(data))
+            if message:
+                return jsonify({"message": message}), 200
+            else:
+                return jsonify({"message": "The data field is in the wrong"}), 400
+
+    except Exception as erro:
+        app.logger.error(erro)
+        return str(0)
+
+
+# 修改历届选举信息
+@mod.route('/update_election/', methods=['POST'])
+def update_election():
+    try:
+        datas = request.form.get('data', '')
+        print(datas)
+        if datas == '':
+            return jsonify({"message": "type input is null"}), 406
+        else:
+            message = update_election_info(json.loads(datas))
+            if message:
+                return jsonify({"message": message}), 200
+            else:
+                return jsonify({"message": "The data field is in the wrong"}), 400
+
+    except Exception as erro:
+        app.logger.error(erro)
+        return str(0)
+
+
+# 搜索全部历届选举信息
+@mod.route('/select_election/',methods=['GET'])
+def select_election():
+    try:
+        message = select_election_all()
+        if message:
+            return jsonify({"message": message}), 200
+        else:
+            return jsonify({"message": "The data field is in the wrong"}), 400
+    except Exception as erro:
+        app.logger.error(erro)
+        return str(0)
+
+
+# 根据id搜索一条选举信息
+@mod.route('/select_election_one/', methods=['POST'])
+def select_election_one():
+    try:
+        datas = request.form.get('data','')
+        if datas == '':
+            return jsonify({"message": "type input is null"}), 406
+        else:
+            message = select_election_info_one(json.loads(datas))
+            if message:
+                return jsonify({"message": message}), 200
+            else:
+                return jsonify({"message": "The data field is in the wrong"}), 400
+
+    except Exception as erro:
+        app.logger.error(erro)
+        return str(0)
+
+
+# 分页查询历届选举信息
+@mod.route('/select_election_page/', methods=['POST'])
+def select_election_page():
+    try:
+        datas = request.form.get('data','')
+        datas_info = json.loads(datas)
+        page = datas_info['page']
+        count = datas_info['count']
+
+        if page and count:
+            message = select_election_info_page(page, count)
+            return jsonify({"message": message}), 200
+        else:
+            return jsonify({"message": "type input is null"}), 406
     except Exception as erro:
         app.logger.error(erro)
         return str(0)
